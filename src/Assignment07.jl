e38c282395d7694e5664625a4e308afe38b1ac9a
Anya Kopyra <kopyra.anya@gmail.com>
2020-07-14 10:32:24 -0400

    Assignment07 attempt 2

--- src/Assignment07.jl
+++ src/Assignment07.jl
@@ -26,4 +26,60 @@ function normalizeDNA(seq)
     return seq # change to `return LongDNASeq(seq)` if you want to try to use BioSequences types
 end
 
+function composition(sequence)
+    sequence = normalizeDNA(sequence)
+
+    counts = Dict(b=>0 for b in collect("ATGCN"))
+    for base in sequence
+        counts[base] += 1
+    end
+
+    return counts
+end
+
+function gc_content(seq)
+    comp = composition(seq)
+    gc= (comp['G'] + comp['C'] )/ length(seq)
+    return gc
+end
+
+function complement(base::Char)
+    complements = Dict('A'=>'T',
+                    'T'=>'A',
+                    'G'=>'C',
+                    'C'=>'G',
+                    'N'=>'N')
+    c= complements[uppercase(base)]
+    return c
+end
+
+
+function complement(base::AbstractString)
+    return join(map(complement, collect(base)))
+end
+
+reverse_complement(seq) = reverse(complement(seq))
+
+function parse_fasta(path)
+    headers = String[]
+    seqs = String[]
+    curseq = String[]
+    for line in eachline(path)
+        if startswith(line, ">")
+            push!(headers, line[2:end])
+            if length(curseq) > 0
+                push!(seqs, join(curseq))
+                curseq = String[]
+            end
+        else
+            line = normalizeDNA(line)
+            push!(curseq, line)
+        end
+    end
+    length(curseq) > 0 && push!(seqs, join(curseq))
+    return headers, seqs
+end
+
+
+
 end # module Assignment07
